7fa50c7c3438f78cf18bd65472356400bcb82383
kyr1234 <83321546+kyr1234@users.noreply.github.com>
2021-09-05 23:50:49 +0530

    Update Row.js

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,54 +1,65 @@
-import React,{useState,useEffect} from 'react';
-import './Row.css';
+import React, { useState, useEffect } from "react";
+import "./Row.css";
 import axios from "./axios.js";
 import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
+function Row({ title, url, isLarge }) {
+  const [movies, setmovies] = useState();
 
-function Row({title,url,isLarge}) {
+  const [trailerurl, settrailerurl] = useState();
 
-const [movies,setmovies]=useState();
+  const opts = {
+    height: "400px",
+    width: "100%",
+    playerVars: {
+      autoplay: 1
+    }
+  };
 
-const opts={
+  const handleClick = (movie) => {
+    if (trailerurl) {
+      settrailerurl("");
+    } else {
+      movieTrailer(movie?.name || " ")
+        .then((url) => {
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          settrailerurl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
-height:400px;
-width:100%;
-autoplay:1
-
-
-}
-
-useEffect(()=>{
-
-const fetchdata=async()=>{
-
-const request=await axios.get(url);
-setmovies(request.data.results);
-}
-fetchdata();
-},[url])
-
-console.log(movies);
-
+  useEffect(() => {
+    const fetchdata = async () => {
+      const request = await axios.get(url);
+      setmovies(request.data.results);
+    };
+    fetchdata();
+  }, [url]);
 
   return (
     <div className="Row">
+      <h2>{title}</h2>
 
-<h2>{title}</h2>
+      <div className="hello">
+        {movies &&
+          movies.map((movie) => (
+            <img
+              onClick={() => handleClick(movie)}
+              className={`poster ${isLarge && "Large_poster"}`}
+              key={movie.id}
+              src={` https://image.tmdb.org/t/p/original${
+                isLarge ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.original_name}
+            />
+          ))}
+      </div>
 
-<div className='hello'>
-{
-  movies && movies.map((movie)=>(
-<img className={`poster ${isLarge && "Large_poster"}`} key={movie.id}  src={` https://image.tmdb.org/t/p/original${isLarge? movie.poster_path:movie.backdrop_path}`} alt={movie.original_name}/>
-
-))}
-
-<Youtube  videoId={targetUrl} opts={opts}    />
-
-
-
-</div>
-
-  </div>
+      {trailerurl && <Youtube videoId={trailerurl} opts={opts} />}
+    </div>
   );
 }
 
