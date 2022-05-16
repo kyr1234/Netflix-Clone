ad20f5f73008dde3d337c0b5a07237542d35143b
Yug KHOKHAR <83321546+kyr1234@users.noreply.github.com>
2022-05-16 11:49:18 +0530

    changes fdf CHANGES IN NETFLIX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Row from "./Components/Row";
 import request from "./MovieApi.js";
 import Banner from "./Components/Banner";
@@ -8,19 +8,15 @@ import Nav from "./Components/Nav";
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Banner />
 
-<Nav/>
-
-
-
-<Banner/>
-
-<Row title="NETFLIX ORIGINALS"url={request.NETFLIXORIGINALS} isLarge/>
-<Row title="TRENDING NOW"url={request.TRENDING} />
-<Row title="TOPRATED" url={request.TOPRATED}/>
-<Row title="UPCOMING"url={request.UPCOMING}/>
-<Row title="HORROR" url={request.HORROR}/>
-<Row title="ROMANCE"  url={request.ROMANCE}/>
+      <Row title="NETFLIX ORIGINALS" url={request.NETFLIXORIGINALS} isLarge />
+      <Row title="TRENDING NOW" url={request.TRENDING} />
+      <Row title="TOPRATED" url={request.TOPRATED} />
+      <Row title="UPCOMING" url={request.UPCOMING} />
+      <Row title="HORROR" url={request.HORROR} />
+      <Row title="ROMANCE" url={request.ROMANCE} />
     </div>
   );
 }
